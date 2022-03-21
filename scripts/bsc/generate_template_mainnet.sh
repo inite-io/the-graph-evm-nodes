86c61c2bc79b64efad125d181681cdb0f66050fe
Myroslav Gavryliak <myroslav.gavriliak@gmail.com>
2022-03-21 12:06:22 +0200

    set correct binance production network

--- scripts/bsc/generate_template_mainnet.sh
+++ scripts/bsc/generate_template_mainnet.sh
@@ -12,4 +12,4 @@ rm ./subgraph.yaml || true
 cp ./subgraph_template.yaml ./subgraph.yaml 
 sed -i '' -e "s/{{ADDRESS}}/0x6d38D1501F7aFb5Db6ca705DFAD5e51C4438e148/" ./subgraph.yaml 
 sed -i '' -e "s/{{START_BLOCK}}/15966414/" ./subgraph.yaml
-sed -i '' -e "s/{{CHAIN}}/bnb/" ./subgraph.yaml
+sed -i '' -e "s/{{CHAIN}}/bsc/" ./subgraph.yaml
